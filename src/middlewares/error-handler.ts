4a2cde477af47988227b78350d3e9af176673641
Neha Demirci <neylanjewar@gmail.com>
2023-07-24 22:53:23 +0200

    add mongoose CASTERROR to error handler

--- src/middlewares/error-handler.ts
+++ src/middlewares/error-handler.ts
@@ -1,18 +1,25 @@
-import { CustomError, GenericError } from '@/errors';
+import { CustomError, GenericError, NotFoundError } from '@/errors';
 import logger from '@/logger';
 import { type NextFunction, type Request, type Response } from 'express';
+
+const MONGOOSE_NOT_FOUND_ERROR = 'CastError';
 
 export const errorHandler = (
   err: Error,
   req: Request,
   res: Response,
-  next: NextFunction,
+  next: NextFunction
 ) => {
   if (err instanceof CustomError) {
     if (err.statusCode >= 500) {
       logErrors(err);
     }
     return res.status(err.statusCode).json(err.serializeErrors());
+  }
+
+  if (err instanceof Error && err.name === MONGOOSE_NOT_FOUND_ERROR) {
+    const error = new NotFoundError();
+    return res.status(error.statusCode).json(error.serializeErrors());
   }
 
   logErrors(err);
